8eddbcbdf6ef78aecf4907bb027cf0ed469cba43
jiawei <jiaweiwu@brandeis.edu>
2024-05-01 20:28:09 -0400

    edit train

--- model/train.py
+++ model/train.py
@@ -164,28 +164,3 @@ def train_model(n_epochs, training_loader, validation_loader, model,
 ckpt_path = "curr_ckpt.pt"
 best_model_path = "best_model.pt"
 trained_model = train_model(EPOCHS, train_data_loader, val_data_loader, model, optimizer, ckpt_path, best_model_path)
-
-example = "Hello, how are you?"
-encodings = tokenizer.encode_plus(
-    example,
-    None,
-    add_special_tokens=True,
-    max_length=MAX_LEN,
-    padding='max_length',
-    return_token_type_ids=True,
-    truncation=True,
-    return_attention_mask=True,
-    return_tensors='pt'
-)
-model.eval()
-with torch.no_grad():
-    input_ids = encodings['input_ids'].to(device, dtype=torch.long)
-    attention_mask = encodings['attention_mask'].to(device, dtype=torch.long)
-    token_type_ids = encodings['token_type_ids'].to(device, dtype=torch.long)
-    output = model(input_ids, attention_mask, token_type_ids)
-    final_output = torch.sigmoid(output).cpu().detach().numpy().tolist()
-    print(final_output)
-    print(train_df.columns[1:].to_list()[int(np.argmax(final_output, axis=1))])
-    potential_emoji = [columns[i] for i in range(len(final_output[0])) if final_output[0][i] > 0.5]
-    for emoji in potential_emoji:
-        print(emoji, " ")
